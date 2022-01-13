e7f340f97c29c7dcfa4ab36f8839df54aff1d7ed
saretoduncan <saretoduncan@gmail.com>
2022-01-13 15:32:05 +0300

    add swagger

--- index.js
+++ index.js
@@ -2,11 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import userRouter from "./src/routers/userRouter.js";
-
+import swaggerUi from "swagger-ui-express";
+import swaggerJSDoc from "swagger-jsdoc";
 const app = express();
 
+const options = {
+  definition: {
+    openapi: "3.0.3",
+    info: {
+      title: "user id generator",
+      version: "3.0.3",
+      description: "get user id as a response by enterering a name as a param",
 
+      contact: {
+        name: "Duncan",
 
+        email: "duncan.moiyo@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./src/routers/*.js"],
+};
+
+const specs = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 dotenv.config();
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,7 +2,46 @@ import express from "express";
 import { getAllUsers } from "../controllers/usersController.js";
 
 const router = express.Router();
-
+/**
+ * @swagger
+ * /api/{name}:
+ *   get:
+ *     summary: Retrieve a single JSONPlaceholder user.
+ *     description: Retrieve a single JSONPlaceholder user. Can be used to populate a user profile when prototyping or testing an API.
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         required: true;
+ *         description: username
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: All users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:                    
+ *                     name:
+ *                       type: string
+ *                       description: The user's name.
+ *                       example: Leanne Graham
+ *                     id:
+ *                       type: string
+ *                       description: The user ID.
+ *                       example: dkjiak-weiht398u48
+ *                     date:
+ *                        type: string
+ *                        description: time user created
+ *                        example: Thu Jan 13 2022 15:25:46 GMT+0300 (East Africa Time)
+ *                      
+ * 
+ *                     
+ */
 
 router.get("/:name", getAllUsers);
 export default router;
